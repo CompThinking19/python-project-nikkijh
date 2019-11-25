90b386acd4a21967f7219d684876f34abb3fe4f4
Nikkita Hovell <njh49@scarletmail.rutgers.edu>
2019-11-25 16:03:32 -0500

    code review two
    I know it won't count now because it's late but it never uploaded. Still submitting it.

--- Pythonproject.py
+++ Pythonproject.py
@@ -21,4 +21,30 @@ textfile = file.read()
 typesofspeech = getwordtypes(textfile)
 #calls getwordtypes on the textfile
 print typesofspeech
-#prints out the types of speech 
+#prints out the types of speech
+
+
+print ('A1. A persons condition with regard to their {oneadjective} and {twoadjective}.' .format(
+    oneadjective = '(oneadjective)' , twoadjective = '(twoadjective)'))
+print ('A2. One in {threenumber} students have a diagnosable illness.' .format(
+    threenumber = '(threenumber)'))
+print ('A3. {fournumber} percent of students have become so anxious that they struggled in school.' .format(
+    fournumber = '(fournumber)'))
+print ('A4. There are three main kinds of symptoms for depression: {fiveadjective} symptoms, {sixadjective} symptoms, and {sevenadjective} symptoms.' .format(
+    fiveadjective = '(fiveadjective)' , sixadjective = '(sixadjective)' , sevenadjective = '(sevenadjective)'))
+
+#This will print out the sentence with the blank so people can try to fill it in with what they think the correct answer is
+
+
+
+
+print ('B1. A persons condition with regard to their {oneadjective} and {twoadjective}.' .format(
+    oneadjective = 'psychological' , twoadjective = 'emotional'))
+print ('B2. One in {threenumber} students have a diagnosable illness.' .format(
+    threenumber = 'four'))
+print ('B3. {fournumber} percent of students have become so anxious that they struggled in school.' .format(
+    fournumber = 'Fifty'))
+print ('B4. There are three main kinds of symptoms for depression: {fiveadjective} symptoms, {sixadjective} symptoms, and {sevenadjective} symptoms.' .format(
+    fiveadjective = 'physical' , sixadjective = 'emotional' , sevenadjective = 'thinking'))
+
+    #This will print out the sentence with the correct answers and then can be compared to what they thought the answer was
