234aeba350d3d02181e8497aed59e04f2d97398c
Nikkita Hovell <njh49@scarletmail.rutgers.edu>
2019-11-25 22:30:00 -0500

    Update Pythonproject.py

--- Pythonproject.py
+++ Pythonproject.py
@@ -24,6 +24,11 @@ print typesofspeech
 #prints out the types of speech
 
 
+
+
+
+
+
 print ('A1. A persons condition with regard to their {oneadjective} and {twoadjective}.' .format(
     oneadjective = '(oneadjective)' , twoadjective = '(twoadjective)'))
 print ('A2. One in {threenumber} students have a diagnosable illness.' .format(
@@ -34,10 +39,16 @@ print ('A4. There are three main kinds of symptoms for depression: {fiveadjectiv
     fiveadjective = '(fiveadjective)' , sixadjective = '(sixadjective)' , sevenadjective = '(sevenadjective)'))
 
 #This will print out the sentence with the blank so people can try to fill it in with what they think the correct answer is
+#I want people to read these facts about mental health and try to fill in the missing parts to test their knowledge abnout this subject because not many people take it serious
 
 
 
 
+
+
+
+
+print 'Correct answers are listed below'
 print ('B1. A persons condition with regard to their {oneadjective} and {twoadjective}.' .format(
     oneadjective = 'psychological' , twoadjective = 'emotional'))
 print ('B2. One in {threenumber} students have a diagnosable illness.' .format(
@@ -48,3 +59,4 @@ print ('B4. There are three main kinds of symptoms for depression: {fiveadjectiv
     fiveadjective = 'physical' , sixadjective = 'emotional' , sevenadjective = 'thinking'))
 
     #This will print out the sentence with the correct answers and then can be compared to what they thought the answer was
+# after they submitted their answers, I want them to see the correct answers to see how close they were to answering it correctly and it will show how much they know. Hopefully by seeing teh correct facts about mental health, it will make them realixe more on how import mental health is
